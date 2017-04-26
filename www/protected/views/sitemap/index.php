30fed86210ba8cfc008fecd52c4987a9109e4cde
XorenBalasanyan <xorenbalasanyan@mail.ru>
2017-04-27 01:18:17 +0400

    Update index.php

--- www/protected/views/sitemap/index.php
+++ www/protected/views/sitemap/index.php
@@ -8,7 +8,11 @@ echo '<?xml version="1.0" encoding="UTF-8"?>'
 		xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 		xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
 				http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-
+	<url>
+        	<loc><?php echo $host; ?></loc>
+        	<changefreq>daily</changefreq>
+		<priority>1</priority>
+	</url>
     <?php foreach ($items as $item): ?>
     <url>
         <loc><?php echo $host; ?><?php echo $item->getUrl(); ?></loc>
